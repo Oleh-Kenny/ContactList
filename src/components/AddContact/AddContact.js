b0cd1243c56586f027bf1d51ffac026db33c9cbe
Oleh <lemeholeh@gmail.com>
2019-12-12 16:46:34 +0200

    addcontact forms

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,10 +1,45 @@
 import React from "react";
+import "./AddContact.css";
+const AddContact = () => {
+    return(
+        
+        <form>
+  <div class="row d-flex">
+    <div class="col-lg-7">
+    <label for="exampleInputFirstName">First Name</label>
+      <input type="text" class="form-control" placeholder="First name" />
+    </div>
+    <div class="col-lg-7">
+    <label for="exampleInputLastName">Last Name</label>
+      <input type="text" class="form-control" placeholder="Last name" />
+    </div>
+  
+  <div class="form-group col-lg-7 top">
+    <label for="exampleInputDiscription">Discription</label>
+    <input type="discription" class="form-control" id="exampleInputDiscription" placeholder="Discription" />
+  </div>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlFile1">Add You Avatar Photo</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+  </div>
+  
+  <label>Gender</label>
+  <select class="form-control form-control-lg col-lg-7">
+  <option>Man</option>
+  <option>Wooman</option>
+</select>
+<div class="form-check cl">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1">You not Bot?</label>
+  </div>
+  <button type="submit" class="btn btn-primary cl">Submit</button>
+  
 
-class AddContact extends React.Component{
-    render (){
-       return <h2>Add Contact</h2>;
-    }
-    
+</form>
 
+        
+    )
 }
+
 export default AddContact;
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,8 +3,8 @@ import {BrowserRouter as Router, Link} from "react-router-dom";
  const MainMenue = () => {
      return(
          
-         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-             <ul className="navbar-nav w-100 ">
+         <nav className="navbar navbar-expand-lg col-lg-6 offset-3 navbar-light bg-light d-flex justyfi-content-center">
+             <ul className="navbar-nav w-100 d-flex justify-content-center ">
                  <li className="nav-item active"><Link  to="/"  >Home</Link></li>
                  <li className="nav-item active"><Link to="about"  >About</Link></li>
                  <li className="nav-item active"><Link to="add"  >Add Cont</Link></li>
